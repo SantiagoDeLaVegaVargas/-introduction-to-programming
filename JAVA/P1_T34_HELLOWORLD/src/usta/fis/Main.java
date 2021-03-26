434ace0d2f4456086ad0fa8c15f81d12cd61d33f
Santiago De La Vega Vargas <santiago.delavegavargas@gmail.com>
2021-03-26 16:19:25 +0100

    Add files via upload

---
+++ JAVA/P1_T34_HELLOWORLD/src/usta/fis/Main.java
@@ -0,0 +1,15 @@
+package usta.fis;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------------------------");
+        System.out.println("╔══════════════════════════╗");
+        System.out.println("║Maker: Santiago De La Vega║");
+        System.out.println("╚══════════════════════════╝");
+        System.out.println("this software show Hello World");
+        System.out.println("--------------------------");
+        System.out.println("HELLO WORLD");
+    }
+}
---
+++ JAVA/P1_T35_INPUT_NUMBER/src/usta/fis/Main.java
@@ -0,0 +1,40 @@
+package usta.fis;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("╔══════════════════════════╗");
+        System.out.println("║Maker: Santiago De La Vega║");
+        System.out.println("╚══════════════════════════╝");
+
+
+        System.out.println("_____SSSS");
+        System.out.println("____SSSSSSS");
+        System.out.println("___SSSSSSS_S");
+        System.out.println("__SSSSSSSSSS");
+        System.out.println("___SSSSSSSSSS");
+        System.out.println("__SSSSSS_SSSSS");
+        System.out.println("__SSSSSSS__SSS");
+        System.out.println("__SSSSSSSS__S");
+        System.out.println("___SSSSSSSSSS");
+        System.out.println("__SSSSSSSSSS");
+        System.out.println("__SSSSSSSSSSS");
+        System.out.println("___SSSSSSSSSSS");
+        System.out.println("SSSSSSSSSSSSSS");
+        System.out.println("__SSSSSSSSSSSSSS");
+        System.out.println("_SSSSSSSSSSSSSS");
+        System.out.println("__SSSSSSSSSSSS");
+        System.out.println("SSSSSSSSSSSS");
+        System.out.println("_S_SSSSSS");
+        System.out.println("__SSSSSS");
+        System.out.println("__SSSSS");
+        System.out.println("__SSSSSS__S");
+        System.out.println("__SSSSSS__SS");
+        System.out.println("_SSSSS____SS");
+        System.out.println("_SSSSS____SS");
+        System.out.println("__SSSSS___SSS");
+        System.out.println("___SSSSSSSSS");
+        System.out.println("____SSSS");
+    }
+}
